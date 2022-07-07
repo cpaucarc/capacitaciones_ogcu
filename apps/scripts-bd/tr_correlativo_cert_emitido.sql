b1193360939f63afefffe8ae47aef1501719257f
Frank Paucar Colonia <52868996+cpaucarc@users.noreply.github.com>
2022-07-07 09:54:34 -0500

    :pencil: actualiza las funciones y triggers
    NOTA: 
    - funciona la ejecucion desde la consola de postgress y DBeaver.
    - no funciona si se ejecuta en HeidiSQL (No reconoce el caracter de $$)

--- apps/scripts-bd/tr_correlativo_cert_emitido.sql
+++ apps/scripts-bd/tr_correlativo_cert_emitido.sql
@@ -1,12 +1,23 @@
+# Ejecutar primero
+CREATE SEQUENCE public.correlativo_cert_secuencia
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+    
+    
+# Ejecutar segundo
 CREATE OR REPLACE FUNCTION public.genera_correlativo_cert()
- RETURNS trigger
- LANGUAGE plpgsql
-AS $function$
+RETURNS trigger AS $$
 BEGIN
   NEW.correlativo := (SELECT LPAD(nextval('correlativo_cert_secuencia') :: TEXT, 5, '0') || '-' || date_part('year', now()));
   RETURN NEW;
-END;$function$;
+END;
+$$ LANGUAGE plpgsql;
 
+
+# Ejecutar tercero
 CREATE TRIGGER trg_correlativo_cert
     BEFORE INSERT
     ON public.capacitacion_certemitido
